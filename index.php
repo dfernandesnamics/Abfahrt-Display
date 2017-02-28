de8edfea686d05d1cee2b19d30264aaa0d10b1e4
Daniel Fernandes <daniel.fernandes@namics.com>
2017-02-28 15:38:11 +0100

    Umbau mit Daten Quelle fertig, Sort - noch dran

--- index.php
+++ index.php
@@ -61,65 +61,82 @@
 		"<div class='endhaltestelle col-xs-6'>" . '%s' . "</div>" .
 		"<div class='wartezeit col-xs-pull-2 col-xs-3 '>" . '%d\'' . "</div></div>";
 
-	$GLOBALS['busankunftformat'] = "<div class='row'><div class='tramnr col-xs-offset-2 col-xs-1'>" . '%s' . "</div>" .
+	$GLOBALS['tramankunftformat'] = "<div class='row'><div class='tramnr col-xs-offset-2 col-xs-1'>" . '%s' . "</div>" .
 		"<div class='endhaltestelle col-xs-6'>" . '%s' . "</div>" .
 		"<div class='wartezeit col-xs-pull-2 col-xs-3'><div class='bus-parent'><div class='icon-bus3'></div></div></div></div>";
 
 
 	class Haltestelle
 	{
-		function wartezeitVorbereitung($tramnr, $wartezeit)
+		function wartezeitVorbereitung($tramnr)
 		{
+
 			$arr = [
-				17
-				=>
-					[
+				17 => [
 
-						"1204", "1211", "1218", "1225", "1232", "1239", "1246", "1253",
-						"1300", "1307", "1314", "1321", "1328", "1335", "1342", "1349", "1356",
-						"1403", "1410", "1417", "1424", "1431", "1438", "1445", "1452", "1459",
-					],
-				5
-				=>
-					[
+					"1025", "1030", "1032", "1225", "1232", "1239", "1246", "1253",
+					"1300", "1307", "1314", "1321", "1328", "1335", "1342", "1349", "1356",
+					"1403", "1410", "1417", "1424", "1431", "1438", "1445", "1452", "1530"
+				],
+				5 => [
 
-						"2206", "2213", "2220", "2227", "2234", "2241", "2248", "2255",
-						"2302", "2309", "2316", "2323", "2330", "2337", "2344", "2351", "2358"
-					],
-				13
-				=>
-					[
-						"1602", "1609", "1616", "1623", "1630", "1637", "1644", "1651", "1658",
-						"1705", "1712", "1719", "1726", "1733", "1740", "1747", "1754",
-						"1801", "1808", "1815", "1822", "1829", "1836", "1843", "1850", "1857",
+					"2206", "2213", "2220", "2227", "2234", "2241", "2248", "2255",
+					"2302", "2309", "2316", "2323", "2330", "2337", "2344", "2351", "2358"
+				],
+				13 => [
+					"1602", "1609", "1616", "1623", "1630", "1637", "1644", "1651", "1658",
+					"1705", "1712", "1719", "1726", "1733", "1740", "1747", "1754",
+					"1801", "1808", "1815", "1822", "1829", "1836", "1843", "1850", "1857"
 
-					],
+				],
 			];
-			for ($i = 0; $i < 100; $i++) {
-				strtotime($arr[13][$i]);
-				if ($GLOBALS['timestamp'] % $arr[13][0] <= $wartezeit) {
-					$this->wartezeitBerechnung($tramnr, $wartezeit);
+			for ($i = 0; $i < count($arr[$tramnr]); $i++) {
+				$tramAbfahrtTime = strtotime($arr[$tramnr][$i]);
+				if ($tramAbfahrtTime > $GLOBALS['timestamp']) {
+					$this->wartezeitBerechnung($tramnr, $tramAbfahrtTime);
+					break;
 				}
 			}
-			echo $GLOBALS['timestamp'] % $arr[13][0];
+
 
 		}
 
-		function wartezeitBerechnung($tramnr, $warteminuten)
+		function wartezeitBerechnung($tramnr, $abfahrtsZeit)
 		{
-			$difference = $GLOBALS['timestamp'] % ($warteminuten * 60);
-			$wartesekunden = ($warteminuten * 60) - $difference;
+			$GLOBALS['warteZeit'] = [];
+			$difference = $abfahrtsZeit - $GLOBALS['timestamp'];
+			array_push($GLOBALS['warteZeit'], $difference);
 
-			$arr = [];
-			$this->printdauer($wartesekunden, $tramnr);
+			print_r($GLOBALS['warteZeit']);
+			$this->sortAbfahrten($difference, $tramnr, $difference);
 
+		}
+
+		function sortAbfahrten($abfahrtsZeit, $tramnr, $difference)
+		{
+			function cmp($a, $b)
+			{
+				if ($a > $b) {
+					return 1;
+				}
+				else{
+					return 0;
+				}
+			}
+
+			uasort($GLOBALS['warteZeit'], 'cmp');
+			foreach ($GLOBALS['warteZeit'] as $key => $val) {
+				echo "$key: $val";
+			}
+
+			$this->printdauer($abfahrtsZeit, $tramnr);
 		}
 
 		function printdauer($wartezeitankunft, $tramnr)
 		{
 			$endhaltestellen = [17 => "Werdhölzli", 13 => "Frankental", 5 => "Laubegg"];
 			if ($wartezeitankunft < 30) {
-				echo sprintf($GLOBALS['busankunftformat'], $tramnr, $endhaltestellen[$tramnr], $wartezeitankunft);
+				echo sprintf($GLOBALS['tramankunftformat'], $tramnr, $endhaltestellen[$tramnr], $wartezeitankunft);
 			} else {
 
 				echo sprintf($GLOBALS['format'], $tramnr, $endhaltestellen[$tramnr], $wartezeitankunft / 60);
@@ -128,13 +145,32 @@
 
 	}
 
-
 	$haltestelle = new Haltestelle;
 
-	$haltestelle->wartezeitBerechnung(17, 7);
-	$haltestelle->wartezeitBerechnung(13, 10);
-	$haltestelle->wartezeitBerechnung(5, 15);
+	$haltestelle->wartezeitVorbereitung(5);
+	$haltestelle->wartezeitVorbereitung(17);
+	$haltestelle->wartezeitVorbereitung(13);
 	?>
 </div>
 </body>
 </html>
+
+<!--$arr = array(-->
+<!--				array('tramnummer' => 17, 'abfahrten' =>[-->
+<!---->
+<!--					"1025", "1030", "1032", "1225", "1232", "1239", "1246", "1253",-->
+<!--					"1300", "1307", "1314", "1321", "1328", "1335", "1342", "1349", "1356",-->
+<!--					"1403", "1410", "1417", "1424", "1431", "1438", "1445", "1452", "1459"-->
+<!--				]),-->
+<!--				array('tramnummer' => 5, 'abfahrten' =>[-->
+<!---->
+<!--					"2206", "2213", "2220", "2227", "2234", "2241", "2248", "2255",-->
+<!--					"2302", "2309", "2316", "2323", "2330", "2337", "2344", "2351", "2358"-->
+<!--				]),-->
+<!--					array('tramnummer' => 13, 'abfahrten' =>[-->
+<!--					"1602", "1609", "1616", "1623", "1630", "1637", "1644", "1651", "1658",-->
+<!--					"1705", "1712", "1719", "1726", "1733", "1740", "1747", "1754",-->
+<!--					"1801", "1808", "1815", "1822", "1829", "1836", "1843", "1850", "1857"-->
+<!---->
+<!--				]),-->
+<!--			);-->
